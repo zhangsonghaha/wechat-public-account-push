fe9af68a6c77ca8a09fef47ede166a86fda37b02
zhangsonghaha <108680296+zhangsonghaha@users.noreply.github.com>
2024-05-12 09:46:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ USE_PASSAGE: 'push-plus',
       // 想要发送的人的名字
       name: '钦钦宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'c57ce44593e3474ba85d7cda80bddcfe',
+      id: 'o49Fk6YIbCxuRPRp_JfSPBjpHMo4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '7gQmzGTr8cSzped2uU5OtBC_6GV78T83UoHlsRMHT5Q',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
