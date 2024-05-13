3aa249b57e28829227f9998dbdfe305027a60570
zhangsonghaha <108680296+zhangsonghaha@users.noreply.github.com>
2024-05-13 21:16:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,7 +49,7 @@ USE_PASSAGE: 'push-plus',
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2024-02-20' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
